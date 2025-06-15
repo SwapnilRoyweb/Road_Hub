09c7e6986f1c2ea1ee97f4d1fd2434ccbac86699
SwapnilRoyWeb <swapnilroyweb@gmail.com>
2025-06-16 00:55:13 +0600

    react router set up done for routing of pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import Main from './Layout/Main.jsx';
+import Home from './Home/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children : [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+
+    </RouterProvider>
   </StrictMode>,
 )
