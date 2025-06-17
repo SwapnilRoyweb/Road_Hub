f60102b656cb510f96041a494080cefae8d94369
SwapnilRoyWeb <swapnilroyweb@gmail.com>
2025-06-17 13:53:46 +0600

    footer added with logo

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import logo from '../../../public/Vibrant_Geometric_Logo_-_Road_Hub-removebg-preview.png'
 
 const Footer = () => {
     return (
-        <div>
-          Footer
+        <div className='flex flex-col md:flex-row-reverse justify-center items-center bg-cyan-950 text-white rounded-2xl'>
+            <img src={logo} alt="" className='w-28 rounded-2xl' />
+            <p className='text-xl text-center'>Copyright © 2025 - All right reserved by</p>
         </div>
-      )
+    )
 }
 
 export default Footer;
