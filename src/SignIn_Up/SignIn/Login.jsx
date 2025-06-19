fa9a5027635ad7ebebd022aaf805f57901dfda06
SwapnilRoyWeb <swapnilroyweb@gmail.com>
2025-06-19 14:55:01 +0600

    login page functionality added

--- src/SignIn_Up/SignIn/Login.jsx
+++ src/SignIn_Up/SignIn/Login.jsx
@@ -1,8 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import background from '../../../public/Vibrant Geometric Logo - Road_Hub.png'
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Providers/Authprovider';
 
 const Login = () => {
+
+    const {signIn} = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/';
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.pass.value;
+
+        signIn(email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            form.reset();
+            navigate(form, {replace: true});
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
 
     return (
         <div className="bg-cover bg-center bg-repeat min-h-screen -ml-5 -mr-10 backdrop-opacity-90" style={{ backgroundImage: `url(${background})` }}>
@@ -11,20 +38,20 @@ const Login = () => {
                 <div className='backdrop-blur-md w-fit h-fit p-10 rounded-2xl'>
                     <h1 className='text-center text-3xl bg-cyan-950 p-3 rounded-md text-white font-bold w-lg border-white border-x-2'>Please, Sign In</h1>
                     {/* email & pass */}
-                    <div className='flex flex-col justify-center  items-center gap-5 mt-10'>
+                    <form className='flex flex-col justify-center  items-center gap-5 mt-10'>
 
                         <div className='flex items-center justify-center gap-3'>
-                            <label for="email" className='text-xl font-bold text-black'>Your Email :</label>
-                            <input type="email" className='w-xs h-10 bg-white rounded-full border-x-2 p-2' />
+                            <label htmlFor="email" className='text-xl font-bold text-black'>Your Email :</label>
+                            <input type="email" name='email' className='w-xs h-10 bg-white rounded-full border-x-2 p-2' />
                         </div>
 
                         <div className='flex items-center justify-center gap-3'>
-                            <label for="email" className='text-xl font-bold text-black '>Password :</label>
-                            <input type="password" className='w-xs h-10 bg-white rounded-full border-x-2 p-2' />
+                            <label htmlFor="email" className='text-xl font-bold text-black '>Password :</label>
+                            <input type="password" name='pass' className='w-xs h-10 bg-white rounded-full border-x-2 p-2' />
                         </div>
 
-                        <button className='w-xs h-14 hover:bg-cyan-900 border-4 border-y-0 border-cyan-950 bg-white text-cyan-950 hover:text-white hover:border-0 text-2xl font-extrabold rounded-full mt-3 flex flex-col items-center justify-center'>Sign In</button>
-                    </div>
+                        <input type='submit' value='SIgn In' className='w-xs h-14 hover:bg-cyan-900 border-4 border-y-0 border-cyan-950 bg-white text-cyan-950 hover:text-white hover:border-0 text-2xl font-extrabold rounded-full mt-3 flex flex-col items-center justify-center'></input>
+                    </form>
                     <p className='text-center mt-5 text-lg text-white font-semibold'>New here? Please <Link to='/signUp' className='font-extrabold text-black hover:text-white'>Sign Up</Link></p>
                 </div>
             </div>
